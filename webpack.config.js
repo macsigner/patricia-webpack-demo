eb6f76eb237a1576b796de67a8eb63a987648122
Mac Signer <signer@mediamotion.ch>
2023-05-09 10:59:22 +0200

    feat | add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import {fileURLToPath} from 'url';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const settings = {
@@ -10,6 +11,23 @@ const settings = {
         path: path.resolve(__dirname, 'dist'),
         assetModuleFilename: 'assets/[name]-[hash][ext]',
     },
+    module: {
+        rules: [
+            {
+                test:  /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'assets/css/[name]-[hash].css',
+        }),
+    ],
 }
 
 export default settings;
