faf416c5aed04cd414fe6ee3388d7c03136f3e26
Mac Signer <signer@mediamotion.ch>
2023-05-09 11:09:35 +0200

    feat | add html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 import * as path from 'path';
 import {fileURLToPath} from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const pages = [
+    'index',
+    'about',
+    'products',
+    'contact',
+]
 
 const settings = {
     mode: 'development',
@@ -26,6 +34,14 @@ const settings = {
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'assets/css/[name]-[hash].css',
+        }),
+        ...pages.map(item => {
+            console.log(item);
+
+            return new HtmlWebpackPlugin({
+                filename: item + '.html',
+                template: `src/pages/${item}.html`,
+            });
         }),
     ],
 }
